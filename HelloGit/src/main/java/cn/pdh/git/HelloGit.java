a8eeed8fc0f8a4760be6a0c3d1b97036bfa2d0ec
pdh6868 <2197627117@qq.com>
2020-12-14 18:04:54 +0800

    克隆项目

--- HelloGit/src/main/java/cn/pdh/git/HelloGit.java
+++ HelloGit/src/main/java/cn/pdh/git/HelloGit.java
@@ -10,5 +10,6 @@ public class HelloGit {
         System.out.println("创建分支");
         System.out.println("主干添加");
         System.out.println("在线更新");
+        System.out.println("克隆下来的项目的添加");
     }
 }
