8aab766c6c3045aaec1466499c7047230b8e560c
pdh6868 <2197627117@qq.com>
2020-12-14 17:37:41 +0800

    添加文件

---
+++ HelloGit/src/main/java/cn/pdh/git/HelloGit.java
@@ -0,0 +1,11 @@
+package cn.pdh.git;
+
+/**
+ * @author pdh
+ * @create 2020-12-14-17:32
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
